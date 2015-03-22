f8d77ae0aaf78929e5445e840c7b9374dbad8799
LOUYOT Edouard <edouard88100@aim.com>
2015-03-22 14:51:24 +0100

    modif vue

--- app/src/main/java/com/example/louyotedouard/iut_gestion/AfficherActivity.java
+++ app/src/main/java/com/example/louyotedouard/iut_gestion/AfficherActivity.java
@@ -5,40 +5,23 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 /**
  * Created by LOUYOT Edouard on 20/03/2015.
  */
 public class AfficherActivity extends ActionBarActivity{
-    private Button btn_addEtudiant,btn_listEtudiant;
+    private ListView list;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu);
+        setContentView(R.layout.afficher);
         addListenerOnButton();
     }
     public void addListenerOnButton() {
-        btn_addEtudiant = (Button) findViewById(R.id.button_addetudiant);
-        btn_listEtudiant = (Button) findViewById(R.id.button_listetudiant);
+        list = (ListView) findViewById(R.id.listView_etudient);
 
-        btn_addEtudiant.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View arg0) {
-                Intent browserIntent =
-                        new Intent(AfficherActivity.this, CandidatActivity.class);
-                startActivity(browserIntent);
-            }
-        });
-        btn_listEtudiant.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                Intent browserIntent =
-                        new Intent(AfficherActivity.this, CandidatActivity.class);
-                startActivity(browserIntent);
-            }
-        });
 
     }
 
